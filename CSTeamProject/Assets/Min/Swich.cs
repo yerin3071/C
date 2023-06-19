57587fce62c2c9fbf0c631b43a434549ea95a975
novebug <zita1122334@gmail.com>
2023-06-19 13:55:07 +0900

    오염수 조금수정

--- CSTeamProject/Assets/Min/Swich.cs
+++ CSTeamProject/Assets/Min/Swich.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class Swich : MonoBehaviour
 {
     public GameObject DangerWater1;
+    
     void Start()
     {
-        
+      
     }
 
     
@@ -21,8 +22,20 @@ public class Swich : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            for(int i =0; i < 100; i++)
+            {
+                DangerWater1.transform.localScale -= new Vector3(0, 0.01f, 0);
+               StartCoroutine(DelayTime());
+            }
+
+
             
-            Destroy(DangerWater1);
         }
+    }
+
+    IEnumerator DelayTime()
+    {
+
+       yield return new WaitForSeconds(10f);
     }
 }
