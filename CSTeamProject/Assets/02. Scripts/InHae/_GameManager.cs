fe55e87a815006d4be4183a606bcb35282e60c58
inhae0519 <lsh266409@gmail.com>
2023-06-22 00:52:13 +0900

    스타트 씬과 타이머 구현

--- CSTeamProject/Assets/02. Scripts/InHae/_GameManager.cs
+++ CSTeamProject/Assets/02. Scripts/InHae/_GameManager.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class _GameManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI timeText;
     public static _GameManager instance = null;
     Player_InHae player;
     Fish fish;
     public bool isGameOver;
+    private float time = 120f;
 
     [SerializeField] GameObject gameOverPanel;
+
+    private void Update()
+    {
+        Timer();
+    }
+
+    private void Timer()
+    {
+        if (time > 0)
+        {
+            if (time < 10)
+            {
+                timeText.color = Color.red;
+            }
+            time -= Time.deltaTime;
+
+            timeText.text = $"{time:N2}";
+        }
+        else
+            GameOver();
+    }
 
     private void Awake()
     {
@@ -22,6 +48,11 @@ public class _GameManager : MonoBehaviour
     {
         isGameOver = true;
         gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        
     }
 
 
